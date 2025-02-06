40a5bfff8627199a59b666c765aa89f5c46a3849
JohannMscs <johann.moscoso@gmail.com>
2025-02-06 16:12:12 -0300

    estruturando e estilizando o componente GameScreen

--- src/components/GameScreen/index.jsx
+++ src/components/GameScreen/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import "./style.css"
 
 export default function GameScreen({verifyLetter}) {
   return (
-    <div>
-        <h1>Você está jogando</h1>
-        <button onClick={verifyLetter}>finalizar jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinha a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adv        inhar uma letra da palavra:</p>
+          <form action="">
+            <input type="text" name='letter' maxLength="1" required />
+            <button>Jogar!</button>
+          </form>
+          <p>Letras já utilizadas</p>
+          <span>a,</span>
+          <span>b,</span>
+        </div>
+      
+
     </div>
   )
 }
